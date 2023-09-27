a878a47061ffabe56de4e8aa2ef8397ed4c5e559
willowsenator <willowsenator@gmail.com>
2023-09-27 19:29:17 +0200

    Add endpoint to getBalance

--- back/app.js
+++ back/app.js
@@ -1,11 +1,19 @@
 const {Web3} = require("web3")
 const web3 = new Web3("http://127.0.0.1:8545")
 const ACCOUNT = "0x00da8e0e602417bb446433cfd8ad1e7f45a6a97e"
+const express = require("express")
+const app = express()
+const PORT = 6700
 
+app.listen(PORT,()=>{
+    console.log("Listening in port: ", PORT)
+})
+
+app.get("/balance", async(req, res)=>{
+    getBalance()
+})
 
 const getBalance = async(account) => {
 	const balance = await web3.eth.getBalance(account)
 	console.log("ETHER: ", web3.utils.fromWei(balance, "ether"))
 }
-
-getBalance(ACCOUNT)
