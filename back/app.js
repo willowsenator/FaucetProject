cc4b622d3e0cac98e60ad058e4b45d339e88a406
willowsenator <willowsenator@gmail.com>
2023-09-28 12:36:46 +0200

    fix transaction faucet endpoint

--- back/app.js
+++ back/app.js
@@ -2,6 +2,8 @@ const {Web3} = require("web3")
 const web3 = new Web3("http://127.0.0.1:8545")
 const express = require("express")
 const fs = require("fs")
+const Tx = require("ethereumjs-tx").Transaction
+
 const app = express()
 const PORT = 6700
 const KEY_FILE = "../custom_blockchain/rpc/keys/chain_authority/UTC--2023-08-29T20-43-20Z--73be6d88-5af0-9b79-ba85-b4bc69253fed"
@@ -17,7 +19,7 @@ app.post("/faucet/:address", async(req, res) => {
      const accountToSend = web.utils.toHex(req.params.address)
      const accountFrom = web3.eth.accounts.decrypt(json, "user0")
 
-     const tx = {
+     const rawTx = {
 	chainId: 8995,
         from: accountFrom.address,
         to: accountToSend,
@@ -25,13 +27,17 @@ app.post("/faucet/:address", async(req, res) => {
         value: web3.utils.toWei("0.1", "ether")
      }
 
-     const txSigned = await accountFrom.signTransaction(tx)
-     const hash = await web3.eth.signTransaction(txSigned.rawTransaction)
-     res.send(hash)
+     var tx = new Tx(rawTx)
+     tx.sign(rawTx)
+     var serializedTx = tx.serialize()
 
-  } catch(err){
-    res.send(err)
-  }
+     console.log("Serialized Tx: ", serializedTx)
+     const response = await web3.eth.sendSignedTransaction(serializedTx)
+     res.json({ transactionHash: response.transactionHash });
+    } catch (err) {
+      console.error('Error:', err);
+      res.status(500).json({ error: 'Transaction failed' });
+    }
 })
 
 app.get("/getBalance/:address", async(req, res)=>{
