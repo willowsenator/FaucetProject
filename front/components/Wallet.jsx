5f5c73b28a2f91456c251fd8d11b4dd014bc17d4
willowsenator <willowsenator@gmail.com>
2023-10-01 20:03:04 +0200

    Modify fetch for getBalance in front

--- front/components/Wallet.jsx
+++ front/components/Wallet.jsx
@@ -2,7 +2,9 @@ import {React, useState, useEffect} from 'react'
 
 function Wallet(){
     const [currentAccount, setCurrentAccount] = useState("0x");
+    const [currentBalance, setCurrentBalance] = useState(0);
     const wallet = window.ethereum;
+    const RPC_URL = "http://127.0.0.1:8545"	
 
     useEffect(() => {
         connectToAccount();
@@ -13,13 +15,17 @@ function Wallet(){
             wallet
                 .request({ method: 'eth_requestAccounts' })
                 .then((accounts) => {
-                    setCurrentAccount(accounts[0]);
+		    const selectedAccount = accounts[0];
+                    setCurrentAccount(selectedAccount);
+                    getBalanceCurrentAccount(selectedAccount);
                 })
                 .catch((error) => {
                     console.error('Failed to retrieve Ethereum accounts:', error);
                 });
             wallet.on('accountsChanged', (accounts)=>{
-                setCurrentAccount(accounts[0]);
+                const selectedAccount = accounts[0];
+                setCurrentAccount(selectedAccount);
+		getBalanceCurrentAccount(selectedAccount);
             });
         } else {
             console.error('Ethereum provider not found. Please install MetaMask or a similar wallet.');
@@ -27,12 +33,23 @@ function Wallet(){
     }
 
     function sendTokens(){
-        console.log("Claiming 0.1 tokens")
+        console.log("Claiming 0.1 tokens");
+	
+    }
+
+    function getBalanceCurrentAccount(){
+        const getBalanceURL = RPC_URL + "/getBalance/" + currentAccount;
+	fetch(getBalanceURL).then((result)=>{
+		setCurrentBalance(result.balance);
+	}).catch((err)=>{
+	   console.error("Error: ", err);	
+	})
     }
     
     return (
         <div>
             <p>Account: <strong>{currentAccount}</strong></p>
+            <p>Balance: <strong>{currentBalance}</strong></p>
             <button onClick={sendTokens}>Claim 0.1 tokens</button>
         </div>
     );
