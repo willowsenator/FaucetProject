e8a38e92d237d8fcd0e73eac783bfc2938f97aa3
willowsenator <willowsenator@gmail.com>
2023-10-01 20:05:09 +0200

    add cors to back

--- back/app.js
+++ back/app.js
@@ -2,8 +2,10 @@ const {Web3} = require("web3");
 const web3 = new Web3("http://127.0.0.1:8545");
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const PORT = 6700;
 const KEY_FILE = "../custom_blockchain/data/keystore/UTC--2023-09-30T10-45-43.872863079Z--b854a30b9651a9d50a847b9c2cf43e975719cd22";
 
